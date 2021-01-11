c5808568cf53677546bda4b36014d4ef9bb37534
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-01-11 15:08:09 -0800

    updated codes

--- Flask/Scrape_Mars.py
+++ Flask/Scrape_Mars.py
@@ -9,93 +9,101 @@ from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 
-def scrape():
-    mars_results = {}
     # Mars news scraping
 
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    news_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
-    browser.visit(news_url)
+   url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+   response = requests.get(url)
 
-    while not browser.is_element_present_by_tag("li", wait_time=5):
-        pass
+   # Create BeautifulSoup object; parse with 'html.parser'
+    soup = bs(response.text, 'html.parser')
 
-    html = browser.html
-    soup = bs(html, 'html.parser')
-    # print(soup.prettify())
-
-    news_title = soup.find("li", class_="slide").find("div", class_="content_title").text
-    news_p = soup.find("li", class_="slide").find("div", class_="article_teaser_body").text
-
-    mars_results["news_title"] = news_title
-    mars_results["news_p"] = news_p
-
-    # Featured image url scraping
-
-    jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    browser.visit(jpl_url)
+    results = soup.find_all('div',class_='slide')
+    results[0]
 
     html = browser.html
     soup = bs(html, 'html.parser')
+    print(soup.prettify())
 
-    featured_img_base = "https://www.jpl.nasa.gov"
-    featured_img_url_raw = soup.find("div", class_="carousel_items").find("article")["style"]
-    featured_img_url = featured_img_url_raw.split("'")[1]
-    featured_img_url = featured_img_base + featured_img_url
+    news_title = soup.find('div',class_='content_title').text
+    print(news_title)
 
-    mars_results["featured_img_url"] = featured_img_url
+    title_results = soup.find_all('div', class_='content_title')
+    nasa_titles = []
+    for result in title_results:
+    nasa_titles.append(result.text.strip())
+    print(nasa_titles)
 
-    # Mars facts scraping
+    news_p = soup.find('div',class_='rollover_description_inner').text
+    print(news_p)
+
+    paragraph_results = soup.find_all('div', class_='rollover_description_inner')
+    nasa_paragraphs = []
+    for result in paragraph_results:
+    nasa_paragraphs.append(result.text.strip())
+    print(nasa_paragraphs)
+
+    # JPL Mars Space Images
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    jplimage_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(jplimage_url)
+
+    browser.click_link_by_partial_text("FULL IMAGE")
+    browser.click_link_by_partial_text("more info")
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    print(soup.prettify())
+
+    # Mars Facts
 
     facts_url = "https://space-facts.com/mars/"
-    tables = pd.read_html(facts_url)
+    facts_tables = pd.read_html(facts_url)
+    df_mars_facts = facts_tables[0]
+    df_mars_facts.columns = ['Description', 'Value']
+    df_mars_facts.set_index('Description', inplace=True)
 
-    facts_df = tables[0]
-    facts_df.columns = ["Fact","Value"]
+    df_mars_facts.to_html('Images/mars_facts.html')
 
-    # get rid of trailing colon
-    facts_df["Fact"] = facts_df["Fact"].str[:-1]
-    facts_df = facts_df.set_index("Fact")
-    facts_df
+    facts_tables = pd.read_html(facts_url)
+    print(facts_tables)
 
-    facts_html_table = facts_df.to_html()
-    facts_html_table = facts_html_table.replace('\n', '')
+    mars_facts = df_mars_facts.to_html(header=True, index=True)
+    print(mars_facts)
+
+    # Mars Hemispheres
+    hemispheres_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemispheres_url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    soup_links = soup.find_all('div', class_="description")
+    marshemisphere = []
+
+    hemtitles = soup.find_all('h3')
+    hemtitle = []
+    for name in hemtitles:
+        print(name.text)
+
+    hemisphere_image_urls = []
+    hemi_dict = {}
+    for link in soup_links:
+        url_hemisphere = 'https://astrogeology.usgs.gov' + link.find('a')['href']
+        print(url_hemisphere)
+        browser.visit(url_hemisphere)
+        time.sleep(1)
     
-    mars_results["facts_html_table"] = facts_html_table
-
-    # Mars hemispheres photo scraping
-
-    base_hemisphere_url = "https://astrogeology.usgs.gov"
-    hemisphere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-    browser.visit(hemisphere_url)
-
     html = browser.html
     soup = bs(html, 'html.parser')
-    
-    hemisphere_image_urls = []
+    title = soup.find('h2', class_='title').text
+    img_url = soup.find('img', class_='wide-image')['src']
+    hemi_dict["title"]= title
+    hemi_dict["img_url"]= 'https://astrogeology.usgs.gov' + img_url
+    hemisphere_image_urls.append(hemi_dict.copy())
 
-    links = soup.find_all("div", class_="item")
-
-    for link in links:
-        img_dict = {}
-        title = link.find("h3").text
-        next_link = link.find("div", class_="description").a["href"]
-        full_next_link = base_hemisphere_url + next_link
-        
-        browser.visit(full_next_link)
-        
-        pic_html = browser.html
-        pic_soup = bs(pic_html, 'html.parser')
-        
-        url = pic_soup.find("img", class_="wide-image")["src"]
-
-        img_dict["title"] = title
-        img_dict["img_url"] = base_hemisphere_url + url
-        
-        hemisphere_image_urls.append(img_dict)
-
-    mars_results["hemisphere_image_urls"] = hemisphere_image_urls
-
-    return mars_results
+print(hemisphere_image_urls)
