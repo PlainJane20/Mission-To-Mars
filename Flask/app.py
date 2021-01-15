b25549657d28f6c104e950d48b0947d3766579a9
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-01-15 05:17:30 -0800

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,39 +1,34 @@
 # Import Dependencies 
-from flask import Flask, render_template, redirect 
-from flask_pymongo import pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import scrape_mars
-import os
-import jinja2
 import pymongo
+import os
 
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 db = client.mars_db
 
-# Create an instance of Flask app
+# create instance of Flask app
 app = Flask(__name__)
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
 
-# Create route that renders index.html template and finds documents from mongo
+# create route that renders index.html template
 @app.route("/")
-def home(): 
-# Find data
-mars_info = mars.db.mars_info.find_one()
+def home():
+    mars_data = db.mars_info.find_one()
+    print(mars_data)
+    return render_template("index.html", mars_all= mars_data)
 
-# Return template and data
-return render_template("index.html", mars_all=mars_info)
-
-# Route that will trigger scrape function
 @app.route("/scrape")
-def scrape(): 
+def scrape():
+    # Run the scrape function
+    mars_dict = scrape_mars.scrape_info()
+    # Update the Mongo database using update and upsert=True
+    #  mongo.db.mars_site.update({}, mars_dict, upsert=True)
+    # Redirect back to home page
+    return redirect("/")
 
-mars_info = scrape_mars.scrape_mars_news()
-mars_info = scrape_mars.scrape_mars_image()
-mars_info = scrape_mars.scrape_mars_facts()
-mars_info = scrape_mars.scrape_mars_hemispheres()
-mars_info.update({}, mars_info, upsert=True)
-
-return redirect("/", code=302)
-
-if __name__ == "__main__": 
-app.run(debug= True)
+if __name__ == "__main__":
+    app.run(debug=True)
